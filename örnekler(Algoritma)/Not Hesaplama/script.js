22368426d6df3eeddc2874262d7905301bca91b4
Enes <muzafferselvi521@gmail.com>
2024-11-12 14:41:53 +0300

    yaptım

--- örnekler(Algoritma)/Not Hesaplama/script.js
+++ örnekler(Algoritma)/Not Hesaplama/script.js
@@ -1,15 +1,30 @@
-function notlariHesapla() {
-    // Kullanıcıdan alınan notları al
+function Hesapla() {
+    
     var sinav1 = Number(document.getElementById("not1").value);
     var sinav2 = Number(document.getElementById("not2").value);
+    var sinav3 = Number(document.getElementById("not3").value);
+    
+  
 
-    // Ortalama hesapla
-    var ortalama = (sinav1 + sinav2) / 2;
+   
+    if (sinav1 < 0 || sinav1 > 100 || sinav2 < 0 || sinav2 > 100 || sinav3 < 0 || sinav3 > 100) {
+        document.getElementById("sonuc").innerHTML = "Notlar 0 ile 100 arasında olmalıdır!";
+        return;
+    }
 
-    // Sonuçları yazdır
+    
+    var ortalama = (sinav1 + sinav2 + sinav3) / 3;
+    
+    
     if (ortalama >= 50) {
-        document.getElementById("sonuc").innerHTML = "Geçtiniz. İşte Notunuz: " + ortalama;
+        document.getElementById("sonuc").innerHTML = "Geçtiniz! İşte Notunuz: " + ortalama.toFixed(2);//ortalama.toFixed(2) ifadesi, ortalamanın yalnızca iki ondalıklı kısmını gösterir. 
     } else {
-        document.getElementById("sonuc").innerHTML = "Geçemediniz! İşte Notunuz: " + ortalama;
+        
+        var enDusukNot = 50 - ortalama; 
+
+        
+        document.getElementById("sonuc").innerHTML = 
+            "Geçemediniz! İşte Notunuz: " + ortalama.toFixed(2) + 
+            ". Geçebilmek için diğer sınavlardan almanız gereken not: " + enDusukNot.toFixed(2);
     }
 }
